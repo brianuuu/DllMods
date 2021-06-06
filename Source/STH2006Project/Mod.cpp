d1dee3ef53e12ae4056f9ef634645885771078e3
brianuuu <38166666+brianuuu@users.noreply.github.com>
2021-06-07 00:15:30 +0100

    Initial commit for STH2006 Project code

---
+++ Source/STH2006Project/Mod.cpp
@@ -0,0 +1,24 @@
+#include "Configuration.h"
+
+extern "C" __declspec(dllexport) void Init(ModInfo * modInfo)
+{
+    std::string dir = modInfo->CurrentMod->Path;
+
+    size_t pos = dir.find_last_of("\\/");
+    if (pos != std::string::npos)
+    {
+        dir.erase(pos + 1);
+    }
+    
+    if (!Configuration::load(dir))
+    {
+        MessageBox(NULL, L"Failed to parse Config.ini", NULL, MB_ICONERROR);
+    }
+
+    // Mandatory codes
+    // Patch "Disable Light Dash Hints" by "Hyper"
+    WRITE_MEMORY(0x528A08, uint8_t, 0xE9, 0xC8, 0x00, 0x00, 0x00);
+
+    // Patch "Disable Boost Button Prompt" by "Hyper"
+    WRITE_MEMORY(0x109BC7C, uint8_t, 0xE9, 0x71, 0x01, 0x00, 0x00);
+}
