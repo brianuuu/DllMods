e20ae2e0f35051aac8ea0ab6cbcb6b072deb5605
brianuuu <38166666+brianuuu@users.noreply.github.com>
2021-06-18 20:04:28 +0100

    v1.3 Update
    -Fixed drifting prompts wrongly displayed as bumpers when "Switch Rails w/ Bumpers (Inc. Nav Change)" is enabled

--- Source/NavigationLightdashSound/Mod.cpp
+++ Source/NavigationLightdashSound/Mod.cpp
@@ -5,17 +5,23 @@
 
 #include <Hedgehog.h>
 
+bool m_changedToQS = false;
 HOOK(bool, __fastcall, NavigationMessages, 0x11BC880, void** This, void* Edx, uint32_t* a2, uint32_t* a3)
 {
-    // Chamge Stick Move into Quick Step Bumpers
-    if (a2[0] == 0x16B0E00 && a2[5] == 0xA) // MsgStartCommonButtonSign && Stick Move
+    // Change Stick Move into Quick Step Bumpers
+    if (a2[0] == 0x16B0E00) // MsgStartCommonButtonSign
     {
-        // Stick Move: a2[5] = 0xA, a2[6] = type (2,1,0) why the fuck is the type flipped
-        // QS: a2[4] = 0, a2[8] = 0, a2[12] = type (0,1,2)
-        a2[0] = 0x16AA0C8; // MsgStartQuickStepSign
-        a2[4] = 0;
-        a2[8] = 0;
-        a2[12] = 2 - a2[6];
+        // Stick Move: a2[4] == 0xFFFFFFFF, a2[5] = 0xA, a2[6] = type (2,1,0) why the fuck is the type flipped
+        if (a2[4] == 0xFFFFFFFF && a2[5] == 0xA) // Not drift & Stick Move
+        {
+            // QS: a2[4] = 0, a2[8] = 0, a2[12] = type (0,1,2)
+            a2[0] = 0x16AA0C8; // MsgStartQuickStepSign
+            a2[4] = 0;
+            a2[8] = 0;
+            a2[12] = 2 - a2[6];
+
+            m_changedToQS = true;
+        }
     }
 
     return originalNavigationMessages(This, Edx, a2, a3);
@@ -23,12 +29,19 @@ HOOK(bool, __fastcall, NavigationMessages, 0x11BC880, void** This, void* Edx, ui
 
 HOOK(void, __fastcall, MsgEndCommonButtonSign, 0xE68870, void* This, void* Edx, uint32_t* a2)
 {
-    // Call original for common button
-    originalMsgEndCommonButtonSign(This, Edx, a2);
+    if (m_changedToQS)
+    {
+        // Call for Quick Step Bumpers
+        a2[0] = 0x16AA0D8; // MsgEndQuickStepSign
+        originalMsgEndCommonButtonSign(This, Edx, a2);
 
-    // Call again for Quick Step Bumpers
-    a2[0] = 0x16AA0D8; // MsgEndQuickStepSign
-    originalMsgEndCommonButtonSign(This, Edx, a2);
+        m_changedToQS = false;
+    }
+    else
+    {
+        // Call original for common button
+        originalMsgEndCommonButtonSign(This, Edx, a2);
+    }
 }
 
 extern "C" __declspec(dllexport) void Init(ModInfo * modInfo)
