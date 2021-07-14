a874a22fe7d964d169be38239936d1483133add4
brianuuu <38166666+brianuuu@users.noreply.github.com>
2021-07-14 01:48:03 +0100

    Increase itembox radius

--- Source/STH2006Project/Itembox.cpp
+++ Source/STH2006Project/Itembox.cpp
@@ -1,23 +1,29 @@
 #include "Itembox.h"
 
+float const c_itemboxRadius = 0.56f;
+
 // Have to use ASM to prevent double playing sfx
 uint32_t sub_D5FD10 = 0xD5FD10;
-uint32_t objItemboxPlaySfxReturnAddress = 0xFFF9AF;
-void __declspec(naked) objItemboxPlaySfx()
+uint32_t objItemPlaySfxReturnAddress = 0xFFF9AF;
+void __declspec(naked) objItemPlaySfx()
 {
-    __asm
-    {
-        call    [sub_D5FD10]
+	__asm
+	{
+		call	[sub_D5FD10]
 		call	Itembox::playItemboxSfx
-        jmp     [objItemboxPlaySfxReturnAddress]
-    }
+		jmp		[objItemPlaySfxReturnAddress]
+	}
 }
 
 void Itembox::applyPatches()
 {
 	// Play itembox sfx for 1up and 10ring
 	WRITE_MEMORY(0x011F2FE0, uint32_t, 4002032);
-	WRITE_JUMP(0xFFF9AA, objItemboxPlaySfx);
+	WRITE_JUMP(0xFFF9AA, objItemPlaySfx);
+
+	// Set itembox radius
+	WRITE_MEMORY(0x11F3353, float*, &c_itemboxRadius);
+	WRITE_MEMORY(0xFFF9E8, float*, &c_itemboxRadius);
 }
 
 void Itembox::playItemboxSfx()
