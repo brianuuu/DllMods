fdf34bda08b3c5688acf77d51de94e31d7442b90
brianuuu <38166666+brianuuu@users.noreply.github.com>
2021-11-12 22:12:19 +0000

    [STH2006] Added Now Loading to ImGui

--- Source/STH2006Project/LoadingUI.h
+++ Source/STH2006Project/LoadingUI.h
@@ -14,19 +14,25 @@ public:
 	// ImGui
 	static std::string m_stagePrevious;
 	static std::string m_bottomText;
+	static bool m_drawEnabled;
+	static float m_fadeInTime;
+	static int m_frame;
+	static bool m_showNowLoading;
 	static void draw();
 
 	// Textures
 	static bool m_init;
-	static bool m_drawEnabled;
-	static float m_fadeInTime;
 	static bool initTextures();
 	static PDIRECT3DTEXTURE9 m_backgroundTexture;
 	static PDIRECT3DTEXTURE9 m_stageTexture;
+	static PDIRECT3DTEXTURE9 m_nowLoadingTexture;
+	static PDIRECT3DTEXTURE9 m_arrowTexture;
 	~LoadingUI()
 	{
 		if (m_backgroundTexture)	m_backgroundTexture->Release();
 		if (m_stageTexture)			m_stageTexture->Release();
+		if (m_nowLoadingTexture)	m_nowLoadingTexture->Release();
+		if (m_arrowTexture)			m_arrowTexture->Release();
 	}
 };
 
