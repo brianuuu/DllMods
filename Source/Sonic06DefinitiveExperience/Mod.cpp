6cfcd4c5a87257ce50636e0a4fc88184707d77ed
brianuuu <38166666+brianuuu@users.noreply.github.com>
2022-03-19 18:52:51 +0000

    [S06DE] Added fix homing trail position code

--- Source/Sonic06DefinitiveExperience/Mod.cpp
+++ Source/Sonic06DefinitiveExperience/Mod.cpp
@@ -94,11 +94,12 @@ extern "C" __declspec(dllexport) void Init(ModInfo * modInfo)
     WRITE_NOP(0xDF2385, 1);
 
     // Patch "Longer Blue Trail" by "N69 & Nekit"
-    WRITE_MEMORY(0xE5FB17, uint16_t, 0x9090);
-    WRITE_MEMORY(0xE5FB19, uint16_t, 0x9090);
-    WRITE_MEMORY(0xE5FB1B, uint16_t, 0x9090);
+    WRITE_NOP(0xE5FB17, 6);
     WRITE_MEMORY(0xE5FE10, uint8_t, 0x48);
     WRITE_MEMORY(0xE5FE70, uint8_t, 0x48);
+
+    // Patch "Fix Homing Trail Position" by "Ahremic"
+    WRITE_MEMORY(0xE4F442, uint8_t, 0xF0);
 
     // Patch "Disable Spin Dash on Dash Panels" by "Hyper"
     WRITE_MEMORY(0xE0AC1C, uint8_t, 0xE9, 0x27, 0x01, 0x00, 0x00);
