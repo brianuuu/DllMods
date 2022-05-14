039ad474822dd1b7282d97ef1c080c2cf12e8d22
brianuuu <38166666+brianuuu@users.noreply.github.com>
2022-05-14 11:30:09 +0100

    [S06DE] Disable voice when getting shields

--- Source/Sonic06DefinitiveExperience/VoiceOver.cpp
+++ Source/Sonic06DefinitiveExperience/VoiceOver.cpp
@@ -34,6 +34,9 @@ HOOK(void, __fastcall, VoiceOver_MsgGetItemType, 0xE6D7D0, void* This, void* Edx
 	case 5: // Board
 	case 6: // Rocket Wisp
 	case 7: // Spike Wisp
+	case 8: // Flame Barrier
+	case 9: // Aqua Barrier
+	case 10: // Thunder Barrier
 	case 11:
 	case 12:
 	case 13: // Power stomp
