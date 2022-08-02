172d28d9fa6b03183216d021a4f91c99f77d3ec0
brianuuu <38166666+brianuuu@users.noreply.github.com>
2022-08-02 19:04:23 +0100

    [S06DE] Enabled rank quote on Burning Blaze

--- Source/Sonic06DefinitiveExperience/RankQuote.cpp
+++ Source/Sonic06DefinitiveExperience/RankQuote.cpp
@@ -1,16 +1,26 @@
 #include "RankQuote.h"
 #include "RankRunAnimation.h"
 #include "VoiceOver.h"
+#include "Configuration.h"
 
 HOOK(void, __fastcall, RankQuote_MsgChangeResultState, 0xE692C0, void* This, void* Edx, uint32_t a2)
 {
     uint32_t const state = *(uint32_t*)(a2 + 16);
-    if (state == 3 
-    && !RankRunAnimation::checkCanPlayRunAnimation()
-    && !Common::IsPlayerSuper())
+    if (state == 3)
     {
-        // Play character stage complete voice
-        VoiceOver::playStageCompleteVoice();
+        bool playStageCompleteVoice = true;
+
+        if (Configuration::m_model == Configuration::ModelType::Sonic 
+         || Configuration::m_model == Configuration::ModelType::SonicElise)
+        {
+            playStageCompleteVoice = !RankRunAnimation::checkCanPlayRunAnimation() && !Common::IsPlayerSuper();
+        }
+
+        if (playStageCompleteVoice)
+        {
+            // Play character stage complete voice
+            VoiceOver::playStageCompleteVoice();
+        }
     }
 
     originalRankQuote_MsgChangeResultState(This, Edx, a2);
