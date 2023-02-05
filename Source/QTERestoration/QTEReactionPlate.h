348a160ff706096f2510b91d640688a284901161
brianuuu <38166666+brianuuu@users.noreply.github.com>
2023-02-05 19:31:38 +0000

    [QTE] Added ReactionPlate code by Skyth
    Co-Authored-By: Skyth <19259897+blueskythlikesclouds@users.noreply.github.com>

--- Source/QTERestoration/QTEReactionPlate.h
+++ Source/QTERestoration/QTEReactionPlate.h
@@ -1,5 +1,10 @@
-#pragma once
+/*----------------------------------------------------------*/
+//	Author: Skyth https://github.com/blueskythlikesclouds
+//	Year: 2022
+//	Description: Restore ReactionPlate from Sonic Unleashed
+/*----------------------------------------------------------*/
 
+#pragma once
 class QTEReactionPlate
 {
 public:
