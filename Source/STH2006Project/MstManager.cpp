1ff7cc354ff619f03823791f4a4cca20d69265d3
brianuuu <38166666+brianuuu@users.noreply.github.com>
2025-05-08 00:07:03 +0100

    [STH2006] Added MstManager

---
+++ Source/STH2006Project/MstManager.cpp
@@ -0,0 +1,59 @@
+#include "MstManager.h"
+
+#include "Application.h"
+
+std::mutex MstManager::m_requestMutex;
+std::set<std::string> MstManager::m_mstRequested;
+
+std::shared_mutex MstManager::m_collectionMutex;
+std::map<std::string, mst> MstManager::m_mstCollection;
+
+bool MstManager::IsRequested
+(
+	std::string const& name
+)
+{
+	std::unique_lock lock(m_requestMutex);
+	return m_mstRequested.count(name);
+}
+
+bool MstManager::RequestMst
+(
+	std::string const& name
+)
+{
+	// check or fill m_mstRequested
+	{
+		std::scoped_lock lock(m_requestMutex);
+		if (m_mstRequested.count(name))
+		{
+			return true;
+		}
+
+		m_mstRequested.insert(name);
+	}
+
+	std::unique_lock lock(m_collectionMutex);
+
+	std::string const fullPath = Application::getModDirString() + "Assets\\Languages\\English\\" + name + ".mst";
+	std::string errorMsg;
+	m_mstCollection[name].Load(fullPath, errorMsg);
+
+	return true;
+}
+
+mst::TextEntry MstManager::GetSubtitle
+(
+	std::string const& name, 
+	std::string const& id
+)
+{
+	if (!IsRequested(name))
+	{
+		return mst::TextEntry();
+	}
+
+	std::shared_lock lock(m_collectionMutex);
+	mst const& mstData = m_mstCollection.at(name);
+	return mstData.GetEntry(id);
+}
---
+++ Source/STH2006Project/MstManager.h
@@ -0,0 +1,24 @@
+/*----------------------------------------------------------*/
+///	Author: brianuuuSonic https://github.com/brianuuu
+///	Year: 2025
+///	Description: Manager that manages loading mst files
+/*----------------------------------------------------------*/
+
+#pragma once
+#include "mst.h"
+
+class MstManager
+{
+public:
+	static bool IsRequested(std::string const& name);
+	static bool RequestMst(std::string const& name);
+	static mst::TextEntry GetSubtitle(std::string const& name, std::string const& id);
+
+private:
+	static std::mutex m_requestMutex;
+	static std::set<std::string> m_mstRequested;
+
+	static std::shared_mutex m_collectionMutex;
+	static std::map<std::string, mst> m_mstCollection;
+};
+
