4145d9a2306650726eb475fbf2342c2af35220ab
brianuuu <38166666+brianuuu@users.noreply.github.com>
2025-05-15 17:11:20 +0100

    [S06DE] Added comment

--- Source/Sonic06DefinitiveExperience/NextGenShadow.cpp
+++ Source/Sonic06DefinitiveExperience/NextGenShadow.cpp
@@ -631,7 +631,8 @@ HOOK(void, __fastcall, NextGenShadow_CSonicStateHomingAttackAfterAdvance, 0x1118
             useNextAttack |= NextGenShadow::m_chaosAttackCount > 0 && NextGenShadow::m_chaosAttackBuffered; // next attacks can be buffered
             useNextAttack |= NextGenShadow::m_chaosAttackCount == 0 && NextGenShadow::m_chaosBoostLevel > 0 && isHoldingA; // Chaos Snap immediately goes to next attack
 
-            if (isChaosSnapSearch && NextGenShadow::CheckChaosSnapTarget() && *(uint32_t*)((uint32_t)context + 0x1174) != context->m_HomingAttackTargetActorID)
+            if (isChaosSnapSearch && NextGenShadow::CheckChaosSnapTarget() 
+            && *(uint32_t*)((uint32_t)context + 0x1174) != context->m_HomingAttackTargetActorID) // don't teleport to previously hit target
             {
                 // Chaos Snap immediately goes to next target if found
                 StateManager::ChangeState(StateAction::HomingAttack, *PLAYER_CONTEXT);
