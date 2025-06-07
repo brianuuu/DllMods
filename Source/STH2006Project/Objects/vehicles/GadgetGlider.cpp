ff3e86f3a970f9e2bacab99d7b79717a0e04d607
brianuuu <38166666+brianuuu@users.noreply.github.com>
2025-06-07 13:56:02 +0100

    [STH2006] Glider explode at contact with terrain

--- Source/STH2006Project/Objects/vehicles/GadgetGlider.cpp
+++ Source/STH2006Project/Objects/vehicles/GadgetGlider.cpp
@@ -264,12 +264,12 @@ bool GadgetGlider::SetAddColliders
 	// damage to object
 	uint32_t const typeEnemy = *(uint32_t*)0x1E5E7E8;
 	uint32_t const typeBreakable = *(uint32_t*)0x1E5E77C;
-	uint32_t const typeTerrain = *(uint32_t*)0x1E5E754;
 	uint32_t const typeInsulate = *(uint32_t*)0x1E5E780;
-	uint64_t const bitfield = (1llu << typeEnemy) | (1llu << typeBreakable) | (1llu << typeTerrain);
+	uint64_t const bitfield = (1llu << typeEnemy) | (1llu << typeBreakable);
 	uint32_t const damageID = Common::MakeCollisionID(0, bitfield);
 	hk2010_2_0::hkpBoxShape* bodyEventTrigger = new hk2010_2_0::hkpBoxShape(4.9f, 0.7f, 2.0f);
 	AddEventCollision("Attack", bodyEventTrigger, damageID, true, m_spMatrixNodeTransform);
+	AddEventCollision("Terrain", bodyEventTrigger, *(int*)0x1E0AFAC, true, m_spMatrixNodeTransform);
 	AddRigidBody(m_spRigidBodyMove, bodyEventTrigger, Common::MakeCollisionID((1llu << typeInsulate), 0), m_spMatrixNodeTransform);
 
 	m_spNodeCockpit = boost::make_shared<Sonic::CMatrixNodeTransform>();
@@ -278,6 +278,7 @@ bool GadgetGlider::SetAddColliders
 	m_spNodeCockpit->SetParent(m_spMatrixNodeTransform.get());
 	hk2010_2_0::hkpBoxShape* cockpitEventTrigger = new hk2010_2_0::hkpBoxShape(0.8f, 0.5f, 1.5f);
 	AddEventCollision("Attack", cockpitEventTrigger, damageID, true, m_spNodeCockpit);
+	AddEventCollision("Terrain", cockpitEventTrigger, *(int*)0x1E0AFAC, true, m_spNodeCockpit);
 	AddRigidBody(m_spRigidBodyCockpit, cockpitEventTrigger, Common::MakeCollisionID((1llu << typeInsulate), 0), m_spNodeCockpit);
 
 	// player event collision
@@ -453,6 +454,22 @@ bool GadgetGlider::ProcessMessage
 						m_followData.m_rotation * hh::math::CVector::UnitZ() * m_followData.m_speed * 2.0f
 					)
 				);
+			}
+		}
+		else if (msg.m_Symbol == "Terrain")
+		{
+			if (msg.m_SenderActorID != m_ActorID)
+			{
+				bool isWall = false;
+				SendMessageImm(msg.m_SenderActorID, Sonic::Message::MsgIsWall(isWall));
+				if (isWall)
+				{
+					Explode();
+				}
+				else
+				{
+					TakeDamage(8.0f);
+				}
 			}
 		}
 		return true;
