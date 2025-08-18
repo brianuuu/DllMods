6f17924e84aa96e5ddf0a90e2ca74cec971cf0b5
brianuuu <38166666+brianuuu@users.noreply.github.com>
2025-08-18 23:18:23 +0100

    [STH2006] Shadow tutorial custom events

--- Source/STH2006Project/Objects/cmn/CustomEvent.cpp
+++ Source/STH2006Project/Objects/cmn/CustomEvent.cpp
@@ -16,6 +16,39 @@ void CustomEvent::AddCallback
 	Sonic::CObjectBase::AddCallback(in_rWorldHolder, in_pGameDocument, in_spDatabase);
 
 	// Handle loading changed values
+	if (!m_chaosBoostCanLevelDown)
+	{
+		S06DE_API::SetChaosBoostCanLevelDown(false);
+	}
+
+	if (m_chaosBoostMatchMaxLevel)
+	{
+		S06DE_API::SetChaosBoostMatchMaxLevel(true);
+	}
+
+	if (m_chaosBoostMaxLevel < 3)
+	{
+		S06DE_API::SetChaosBoostMaxLevel(m_chaosBoostMaxLevel);
+	}
+}
+
+void CustomEvent::KillCallback()
+{
+	// Handle reverting changed values
+	if (!m_chaosBoostCanLevelDown)
+	{
+		S06DE_API::SetChaosBoostCanLevelDown(true);
+	}
+
+	if (m_chaosBoostMatchMaxLevel)
+	{
+		S06DE_API::SetChaosBoostMatchMaxLevel(false);
+	}
+
+	if (m_chaosBoostMaxLevel < 3)
+	{
+		S06DE_API::SetChaosBoostMaxLevel(3);
+	}
 }
 
 void CustomEvent::AddParameterBank
@@ -23,6 +56,9 @@ void CustomEvent::AddParameterBank
 	const Hedgehog::Base::CRefPtr<Sonic::CParameterBank>& in_rParameterBank
 )
 {
+	in_rParameterBank->AccessParameterBankBool("ChaosBoostLevelDown", &m_chaosBoostCanLevelDown);
+	in_rParameterBank->AccessParameterBankBool("ChaosBoostMatchMaxLevel", &m_chaosBoostMatchMaxLevel);
+	in_rParameterBank->AccessParameterBankUnsignedInt("ChaosBoostMaxLevel", &m_chaosBoostMaxLevel);
 }
 
 void CustomEvent::SetUpdateParallel
@@ -61,15 +97,69 @@ bool CustomEvent::ProcessMessage
 			}
 			case 2:
 			{
-				// Replace StopPeopleObject start
+				// StopPeopleObject start
 				SendMessageImm(context->m_pPlayer->m_ActorID, Sonic::Message::MsgStopActivity());
 				Common::SetPlayerVelocity(hh::math::CVector::Zero());
 				break;
 			}
 			case 3:
 			{
-				// Shadow StopPeopleObject end
+				// StopPeopleObject end
 				SendMessage(context->m_pPlayer->m_ActorID, boost::make_shared<Sonic::Message::MsgReopenActivity>());
+				break;
+			}
+			case 4:
+			{
+				// Shadow tutorial, stop leveling down
+				m_chaosBoostCanLevelDown = false;
+				S06DE_API::SetChaosBoostCanLevelDown(false);
+				break;
+			}
+			case 5:
+			{
+				m_chaosBoostCanLevelDown = true;
+				S06DE_API::SetChaosBoostCanLevelDown(true);
+				break;
+			}
+			case 6:
+			{
+				// Disable Chaos Boost
+				m_chaosBoostMaxLevel = 0;
+				S06DE_API::SetChaosBoostMaxLevel(0);
+				break;
+			}
+			case 7:
+			{
+				// Chaos Boost Lv.1
+				m_chaosBoostMaxLevel = 1;
+				S06DE_API::SetChaosBoostMaxLevel(1);
+				break;
+			}
+			case 8:
+			{
+				// Chaos Boost Lv.2
+				m_chaosBoostMaxLevel = 2;
+				S06DE_API::SetChaosBoostMaxLevel(2);
+				break;
+			}
+			case 9:
+			{
+				// Chaos Boost Lv.3
+				m_chaosBoostMaxLevel = 3;
+				S06DE_API::SetChaosBoostMaxLevel(3);
+				break;
+			}
+			case 10:
+			{
+				// Shadow tutorial, Chaos Boost match max level
+				m_chaosBoostMatchMaxLevel = true;
+				S06DE_API::SetChaosBoostMatchMaxLevel(true);
+				break;
+			}
+			case 11:
+			{
+				m_chaosBoostMatchMaxLevel = false;
+				S06DE_API::SetChaosBoostMatchMaxLevel(false);
 				break;
 			}
 			}
