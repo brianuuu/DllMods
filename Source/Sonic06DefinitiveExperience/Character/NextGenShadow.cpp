89db903cc805107dea197f52d508102802458625
brianuuu <38166666+brianuuu@users.noreply.github.com>
2025-09-20 11:54:08 +0100

    [S06DE] Fixed Glider spawning upside down

--- Source/Sonic06DefinitiveExperience/Character/NextGenShadow.cpp
+++ Source/Sonic06DefinitiveExperience/Character/NextGenShadow.cpp
@@ -294,7 +294,7 @@ HOOK(void, __fastcall, NextGenShadow_CSonicUpdate, 0xE6BF20, Sonic::Player::CPla
                 // jet glider
                 hh::math::CVector forward = transform.m_Rotation * hh::math::CVector::UnitZ();
                 forward.y() = 0.0f; forward.normalize();
-                if (!forward.isApprox(hh::math::CVector::UnitZ()))
+                if (!forward.isApprox(-hh::math::CVector::UnitZ()))
                 {
                     transform.m_Rotation = hh::math::CQuaternion::FromTwoVectors(hh::math::CVector::UnitZ().head<3>(), forward.head<3>());
                 }
